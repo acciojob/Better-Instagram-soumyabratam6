b23a9c97047ce48906546a402a99558893b45b01
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-05-10 06:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your code here
 // Get all elements with the class name 'image'
 var images = document.querySelectorAll('.image');
 
@@ -6,24 +5,26 @@ var draggedElement = null;
 
 images.forEach(function(image) {
 	image.setAttribute('draggable', true);
-  image.addEventListener('dragstart', function(event) {
-      draggedElement = event.target;
-	  event.dataTransfer.setData('text', event.target.id);
-     event.target.classList.add('selected');
-  });
-});
+  
+  	image.addEventListener('dragstart', function(event) {
+    	draggedElement = event.target;
+	    event.dataTransfer.setData('text', event.target.id);
+     	event.target.classList.add('selected');
+  	});
 
-document.getElementById('parent').addEventListener('dragover', function(event) {
-  event.preventDefault();
-});
-document.getElementById('parent').addEventListener('drop', function(event) {
-  event.preventDefault();
-	var id = event.dataTransfer.getData('text');
-  if (draggedElement) {
-    var tempBackground = draggedElement.style.backgroundImage;
-    draggedElement.style.backgroundImage = event.target.style.backgroundImage;
-    event.target.style.backgroundImage = tempBackground;
-    draggedElement.classList.remove('selected');
-    draggedElement = null;
-  }
+  	image.addEventListener('dragover', function(event) {
+  		event.preventDefault();
+	});
+
+	image.addEventListener('drop', function(event) {
+  		event.preventDefault();
+		var id = event.dataTransfer.getData('text');
+  		if (draggedElement) {
+    		var tempBackground = draggedElement.style.backgroundImage;
+    		draggedElement.style.backgroundImage = event.target.style.backgroundImage;
+    		event.target.style.backgroundImage = tempBackground;
+    		draggedElement.classList.remove('selected');
+    		draggedElement = null;
+  		}
+	});  
 });
