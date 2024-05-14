549fa3ef0e7db6f186f02da28e52115ad5d27a2e
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-05-14 20:47:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-// Get all elements with the class name 'image'
-var images = document.querySelectorAll('.image');
+let draggedItem = null;
 
-var draggedElement = null;
+function allowDrop(event) {
+  event.preventDefault();
+}
 
-images.forEach(function(image) {
-	image.setAttribute('draggable', true);
-  
-  	image.addEventListener('dragstart', function(event) {
-    	draggedElement = event.target;
-	    event.dataTransfer.setData('text', event.target.id);
-     	event.target.classList.add('selected');
-  	});
+function drag(event) {
+  draggedItem = event.target;
+}
 
-  	image.addEventListener('dragover', function(event) {
-  		event.preventDefault();
-		event.dataTransfer.dropEffect = 'move'; 
-	});
-
-	image.addEventListener('drop', function(event) {
-  		 event.preventDefault();
-		  if (draggedElement) {
-		    var tempBackground = draggedElement.style.backgroundImage;
-		    draggedElement.style.backgroundImage = event.target.style.backgroundImage;
-		    event.target.style.backgroundImage = tempBackground;
-		    draggedElement.classList.remove('selected');
-		    draggedElement = null;
-		  }
-	});  
-});
+function drop(event) {
+  event.preventDefault();
+  if (draggedItem !== event.target) {
+    // Swap background images
+    let tempBackground = event.target.style.backgroundImage;
+    event.target.style.backgroundImage = draggedItem.style.backgroundImage;
+    draggedItem.style.backgroundImage = tempBackground;
+  }
+}
